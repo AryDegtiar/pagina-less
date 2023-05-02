2913fc9b27b560e5ae3fa391fb9e011e7918d0a8
AryDegtiar <adegtiar@frba.utn.edu.ar>
2023-05-01 23:31:47 -0300

    model funchionando

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,34 @@
-/* modal */
-function openModal(title, body, customVariable) {
+const modalLinks = document.querySelectorAll('.modal-link');
+
+const proyectos = [
+  {
+    id: 'proyecto1',
+    title: '<b>PROYECTO 1</b>',
+    body: '<img src="./assets/images/1.jpg" class="d-block w-100 object-fit-cover" alt="..."> <div class="row pt-4 ps-2"> <div class="col-12 col-xl-6"> <p> Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 Proyecto 1 </p> </div> <div class="col-12 col-xl-6"> <img src="./assets/images/2.jpg" class="d-block w-100 object-fit-cover" alt="..."> </div> </div> ',
+  },
+  {
+    id: 'proyecto2',
+    title: '<b>PROYECTO 2</b>',
+    body:' <img src="./assets/images/1.jpg" class="d-block w-100 object-fit-cover" alt="..."> <div class="row pt-4 ps-2"> <div class="col-12 col-xl-6"> <p> Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 </p> </div> <div class="col-12 col-xl-6"> <img src="./assets/images/2.jpg" class="d-block w-100 object-fit-cover" alt="..."> </div> </div> ',
+  },
+  {
+    id: 'proyecto3',
+    title: '<b>PROYECTO 3</b>',
+    body:' <img src="./assets/images/1.jpg" class="d-block w-100 object-fit-cover" alt="..."> <div class="row pt-4 ps-2"> <div class="col-12 col-xl-6"> <p> Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 Proyecto 2 </p> </div> <div class="col-12 col-xl-6"> <img src="./assets/images/2.jpg" class="d-block w-100 object-fit-cover" alt="..."> </div> </div> ',
+  }
+];
+
+function openModal(proyecto) {
   console.log('openModal');
   const modalTitle = document.querySelector('.modal-title');
   const modalBody = document.querySelector('.modal-body');
-
-  modalTitle.innerHTML = title;
-  modalBody.innerHTML = body + "<p>Variable personalizada: " + customVariable + "</p>";
-
+  
+  modalTitle.innerHTML = proyecto.title;
+  modalBody.innerHTML = proyecto.body;
+  
   const modalElement = document.getElementById('exampleModal');
   const modal = new bootstrap.Modal(modalElement);
-
+  
   const closeButton = modalElement.querySelector('.btn-close');
   closeButton.addEventListener('click', () => {
     modal.hide();
@@ -25,22 +44,22 @@ function openModal(title, body, customVariable) {
   modalElement.addEventListener('hidden.bs.modal', () => {
     modal.dispose();
     modalElement.classList.remove('modal-backdrop');
-
     document.body.style.overflow = 'initial'; // Elimina el estilo del body
   });
-
+  
   modal.show();
+  document.body.style.overflow = 'hidden';
 }
 
-const launchDemoModal = document.querySelector('.modal-link');
-launchDemoModal.addEventListener('click', (event) => {
-  event.preventDefault();
-  const modalTitle = 'Modal title desde js';
-  const modalBody = `
-    <img src="./images/1.jpg" class="d-block w-100 tamanio-carousel" alt="...">
-    <p>Modal body text goes here.</p>
-  `;
-  const customVariable = launchDemoModal.getAttribute('data-custom-variable');
-
-  openModal(modalTitle, modalBody, customVariable);
+modalLinks.forEach((modalLink) => {
+  modalLink.addEventListener('click', () => {
+    console.log('se hizo click y se entro en modallink');
+    console.log(modalLink.dataset.customVariable);
+    proyectoId = modalLink.dataset.customVariable; // Obtiene el id del proyecto del atributo data-custom-variable del elemento modalLink
+    proyecto = proyectos.find((p) => p.id === proyectoId); // Busca el objeto proyecto correspondiente en el arreglo de proyectos
+    console.log(proyecto);
+    openModal(proyecto);
+  });
 });
+
+
