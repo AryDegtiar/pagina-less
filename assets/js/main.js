cac0fc45fa6aad6dcd882e15848f7ce0b91e6346
AryDegtiar <adegtiar@frba.utn.edu.ar>
2023-05-01 21:48:42 -0300

    se arregla bug del modal al cerrar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,46 @@
 /* modal */
 function openModal(title, body, customVariable) {
-    console.log('openModal');
-    const modalTitle = document.querySelector('.modal-title');
-    const modalBody = document.querySelector('.modal-body');
-  
-    modalTitle.innerHTML = title;
-    modalBody.innerHTML = body + "<p>Variable personalizada: " + customVariable + "</p>";
-  
-    const modalElement = document.getElementById('exampleModal');
-    const modal = new bootstrap.Modal(modalElement);
-  
-    const closeButton = modalElement.querySelector('.btn-close');
-    closeButton.addEventListener('click', () => {
-      modal.hide();
-  
-      const backdropElements = document.querySelectorAll('.modal-backdrop');
-      backdropElements.forEach((element) => {
-        element.remove();
-      });
+  console.log('openModal');
+  const modalTitle = document.querySelector('.modal-title');
+  const modalBody = document.querySelector('.modal-body');
+
+  modalTitle.innerHTML = title;
+  modalBody.innerHTML = body + "<p>Variable personalizada: " + customVariable + "</p>";
+
+  const modalElement = document.getElementById('exampleModal');
+  const modal = new bootstrap.Modal(modalElement);
+
+  const closeButton = modalElement.querySelector('.btn-close');
+  closeButton.addEventListener('click', () => {
+    modal.hide();
+
+    const backdropElements = document.querySelectorAll('.modal-backdrop');
+    backdropElements.forEach((element) => {
+      element.remove();
     });
-  
-    modalElement.addEventListener('hidden.bs.modal', () => {
-      modal.dispose();
-      modalElement.classList.remove('modal-backdrop');
-    });
-  
-    modal.show();
-  }
-  
-  const launchDemoModal = document.querySelector('.modal-link');
-  launchDemoModal.addEventListener('click', (event) => {
-    event.preventDefault();
-    const modalTitle = 'Modal title desde js';
-    const modalBody = `
-      <img src="./images/1.jpg" class="d-block w-100 tamanio-carousel" alt="...">
-      <p>Modal body text goes here.</p>
-    `;
-    const customVariable = launchDemoModal.getAttribute('data-custom-variable');
-  
-    openModal(modalTitle, modalBody, customVariable);
+
+    document.body.style.overflow = 'initial'; // Elimina el estilo del body
   });
-  
-  
+
+  modalElement.addEventListener('hidden.bs.modal', () => {
+    modal.dispose();
+    modalElement.classList.remove('modal-backdrop');
+
+    document.body.style.overflow = 'initial'; // Elimina el estilo del body
+  });
+
+  modal.show();
+}
+
+const launchDemoModal = document.querySelector('.modal-link');
+launchDemoModal.addEventListener('click', (event) => {
+  event.preventDefault();
+  const modalTitle = 'Modal title desde js';
+  const modalBody = `
+    <img src="./images/1.jpg" class="d-block w-100 tamanio-carousel" alt="...">
+    <p>Modal body text goes here.</p>
+  `;
+  const customVariable = launchDemoModal.getAttribute('data-custom-variable');
+
+  openModal(modalTitle, modalBody, customVariable);
+});
